ff3b438c1bbba4a284de507fd413a14086244c1e
wogns <wognscn@naver.com>
2023-08-25 19:31:01 +0900

    8월 25일자 (미완)

--- app/src/main/java/com/example/car_guide_ray/OuterFragment.java
+++ app/src/main/java/com/example/car_guide_ray/OuterFragment.java
@@ -1,21 +1,26 @@
 package com.example.car_guide_ray;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
 import java.util.List;
 
@@ -25,6 +30,9 @@ public class OuterFragment extends Fragment implements CameraBridgeViewBase.CvCa
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
     int counter = 0;
+    int activeCamera = CameraBridgeViewBase.CAMERA_ID_FRONT;
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1001;
 
 
     @Override
@@ -36,14 +44,17 @@ public class OuterFragment extends Fragment implements CameraBridgeViewBase.CvCa
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
 
+
         baseLoaderCallback = new BaseLoaderCallback(getActivity()) {
             @Override
             public void onManagerConnected(int status){
                 super.onManagerConnected(status);
 
                 if (status == BaseLoaderCallback.SUCCESS) {
+                    Log.d("OpenCV", "OpenCV initialization successful");
                     cameraBridgeViewBase.enableView();
                 } else {
+                    Log.e("OpenCV", "OpenCV initialization failed");
                     super.onManagerConnected(status);
                 }
             }
@@ -56,14 +67,15 @@ public class OuterFragment extends Fragment implements CameraBridgeViewBase.CvCa
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
+        Log.d("CameraCallback", "Camera frame received");
         Mat frame = inputFrame.rgba();
 
-        if(counter % 2 == 0){
+        /*if(counter % 2 == 0){
             Core.flip(frame, frame, 1);
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
         }
 
-        counter = counter + 1;
+        counter = counter + 1;*/
 
         return frame;
     }
@@ -86,12 +98,39 @@ public class OuterFragment extends Fragment implements CameraBridgeViewBase.CvCa
         super.onResume();
 
         if(!OpenCVLoader.initDebug()){
+            Log.e("OpenCV", "OpenCV initialization failed");
             Toast.makeText(getActivity(),"There's a problem.", Toast.LENGTH_SHORT).show();
         }
         else{
             baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+
+                }
+                else{
+                    cameraBridgeViewBase.enableView();
+                }
+            }
         }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 카메라 권한 허용
+                cameraBridgeViewBase.enableView();
+            } else {
+                // 카메라 권한 거부
+                Toast.makeText(getActivity(), "Camera permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     @Override
     public void onPause() {
